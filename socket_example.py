b8759ba62d2af502d317d331c61ba91e18caf151
Nina Finstad <finstad83@gmail.com>
2026-03-19 11:48:41 +0100

    Add final README with reflection, instructions and fun output variation

--- socket_example.py
+++ socket_example.py
@@ -6,10 +6,14 @@ hostname = "vg.no"
 ip_address = socket.gethostbyname(hostname)
 # Print result
 print(f"Hello! The IP address of {hostname} is {ip_address}")
-# Testing others and lokal machine
+# Testing others and local machine
 local_machine = socket.gethostname()
 websites = ["nrk.no", "google.com", local_machine]
 # Loop for testing
 for site in websites:
     ip = socket.gethostbyname(site)
     print(f"{site} : {ip}")
+# Trying a more fun and personal output
+for site in websites:
+    ip = socket.gethostbyname(site)
+    print(f"🎉 Yeiii, you made it! {site} lives at {ip}")
