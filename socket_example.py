a0e14818583e93e77b5d524567ac2c6ac5f8c062
Nina Finstad <finstad83@gmail.com>
2026-03-19 11:23:24 +0100

    Replaced example.com with local machine hostname and tested local IP

--- socket_example.py
+++ socket_example.py
@@ -6,8 +6,9 @@ hostname = "vg.no"
 ip_address = socket.gethostbyname(hostname)
 # Print result
 print(f"Hello! The IP address of {hostname} is {ip_address}")
-# Testing others
-websites = ["nrk.no", "google.com", "example.com"]
+# Testing others and lokal machine
+local_machine = socket.gethostname()
+websites = ["nrk.no", "google.com", local_machine]
 # Loop for testing
 for site in websites:
     ip = socket.gethostbyname(site)
