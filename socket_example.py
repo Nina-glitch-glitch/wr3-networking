2f36af61cb6a30792d0ec422b6fcbdfbf7d74f97
Nina Finstad <finstad83@gmail.com>
2026-03-19 11:11:48 +0100

    Tested vg.no and multiple hostnames using a loop

---
+++ socket_example.py
@@ -0,0 +1,14 @@
+# Work Requirement 3 – Networking (Socket Task)
+import socket
+# Hostname to resolve
+hostname = "vg.no"
+# Get IP address from hostname
+ip_address = socket.gethostbyname(hostname)
+# Print result
+print(f"Hello! The IP address of {hostname} is {ip_address}")
+# Testing others
+websites = ["nrk.no", "google.com", "example.com"]
+# Loop for testing
+for site in websites:
+    ip = socket.gethostbyname(site)
+    print(f"{site} : {ip}")
